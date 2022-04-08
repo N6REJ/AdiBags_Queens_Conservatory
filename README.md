6408015b7d9c88f6ba227c33a415a358d374c99c
Bear <programming@hallhome.us>
2022-04-08 04:55:27 -0500

    Initial Seeding

---
+++ README.md
@@ -0,0 +1,17 @@
+# Adibags Queens Conservatory
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+![Size](https://img.shields.io/github/repo-size/N6REJ/AdiBags_Queens_Conservatory) 
+[![Build Curse Release](https://github.com/N6REJ/AdiBags_Queens_Conservatory/actions/workflows/action.yml/badge.svg)](https://github.com/N6REJ/AdiBags_Queens_Conservatory/actions/workflows/action.yml) 
+[![GitHub release](https://img.shields.io/github/release/N6REJ/AdiBags_Queens_Conservatory.svg)](https://GitHub.com/N6REJ/AdiBags_Queens_Conservatory/releases/)
+
+![Shadowlands](https://img.shields.io/badge/Supports-Shadowlands-0B68D7)
+![9.2](https://img.shields.io/badge/Ready_for-9.2-darkgreen)
+[![curse](https://img.shields.io/badge/Curseforge_Project_ID:-446518-purple)](https://www.curseforge.com/wow/addons/adibags_Queens_Conservatory)
+[![requires](https://img.shields.io/badge/Requires-AdiBags-brown)](https://www.curseforge.com/wow/addons/adibags)
+
+sorts "Seeds" & "Catalyst Plots" into their own categories.
+
+
+![settings](https://user-images.githubusercontent.com/1850089/138686951-4f20ed0c-5e8e-4836-b901-feea97929033.png)
+
+![backpack](https://user-images.githubusercontent.com/1850089/138689871-41f53c05-ca48-4325-ba4e-3d1c87d2ed7d.png)
---
+++ database.lua
@@ -0,0 +1,49 @@
+-- AdiBags_Queens_Conservatory - Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://n6rej.github.io
+--
+local addonName, addonTable, addon = ...
+
+-- Create addon table
+local db = { }
+
+db.name = "Shadowlands Fishing"
+db.desc = "Fish and Bait found while fishing in shadowlands"
+
+-- Filter info
+db.Filters = {
+	["Fish"] = {
+		uiName = "Shadowlands Fish",
+		uiDesc = "Fish found while fishing in Shadowlands",
+		title = "Fish",
+		items = {
+			-- ID,	--Item name
+			[173032] = true,	-- Lost Sole
+			[173033] = true,	-- Iridescent Amberjack
+			[173034] = true,	-- Silvergill Pike
+			[173035] = true,	-- Pocked Bonefish
+			[173036] = true,	-- Spinefin Piranha
+			[173037] = true,	-- Elysian Thade
+			[174327] = true,	-- Malformed Gnasher
+			[187702] = true -- Precurser Placoderm
+		},
+	},
+	["Bait"] = {
+		uiName = "Shadowlands Bait",
+		uiDesc = "Bait found while fishing in Shadowlands",
+		title = "Bait",
+		items = {
+			-- ID,	--Item name
+		   [173038] = true,	-- Lose Sole bait
+		   [173039] = true,	-- Iridescent amberjack bait
+		   [173040] = true,	-- Silvergill pike bait
+		   [173041] = true,	-- Pocked Bonefish bait
+		   [173042] = true,	-- Spinefin piranha bait
+		   [173043] = true,	-- Elysian thade bait
+		   [187712] = true -- Precursor Placoderm Bait
+	   },
+	},
+}
+
+-- now that db is populated lets pass it on.
+addonTable.db = db
